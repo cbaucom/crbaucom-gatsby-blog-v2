207fff1504e9ba0705309faf5d8b5188d12d643c
Chris Baucom <crbaucom@gmail.com>
2019-01-29 17:12:48 -0500

    Remove console log

--- src/helpers/uppercase.js
+++ src/helpers/uppercase.js
@@ -1,11 +1,8 @@
 /** https://stackoverflow.com/a/1026087 */
 export default function capitalizeFirstLetter(string) {
-    console.log(string);
-    if (string !== undefined)
-    {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    } else
-    {
-        return null;
-    }
+  if (string !== undefined) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  } else {
+    return null
+  }
 }

bbf528c0932163e8839cd3e043e252b9dc2fa579
Chris Baucom <crbaucom@gmail.com>
2019-01-30 03:07:45 -0500

    Update contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,77 +39,66 @@ export default class Contact extends Component {
       <div id="contact" className="Frontpage__contact">
         <div className="container centered">
           <h2 className="text">Let's build something together!</h2>
+          <p>Need help with your project?</p>
           <p>
-            In need of a beautiful, responsive, progressive web app? Email me
-            and let's discuss how we can make it happen!
+            Is your Wordpress site slow and bogged down with plugins and you
+            can't wait to migrate to <a href="https://gatsbyjs.org">Gatsby?</a>
           </p>
+          <p>
+            Looking to build a progressive web app that works offline and looks
+            great on desktops, tablets, and phones?
+          </p>
+          <p>Contact me and let's discuss how we can make it happen!</p>
+        </div>
 
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <form
-                method="POST"
-                action="https://formspree.io/crbaucom@gmail.com"
-              >
-                <div className="form-group">
-                  <label className="col-sm-3" htmlFor="name">
-                    Name:
-                  </label>
-                  <input
-                    id="name"
-                    className="col-sm-9 form-control"
-                    type="text"
-                    name="name"
-                    placeholder="Chris Baucom"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email" className="col-sm-3">
-                    Email:{' '}
-                  </label>
-                  <input
-                    id="email"
-                    className="col-sm-9 form-control"
-                    type="email"
-                    name="email"
-                    placeholder="email@example.com"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="phone" className="col-sm-3">
-                    Phone:{' '}
-                  </label>
-                  <input
-                    id="phone"
-                    className="col-sm-9 form-control"
-                    type="tel"
-                    name="phone"
-                    placeholder="9035551234"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="message" className="col-sm-3">
-                    Message:{' '}
-                  </label>
-                  <textarea
-                    id="message"
-                    className="col-sm-9 form-control"
-                    type="text"
-                    name="message"
-                    placeholder="I'd love to hire you to build my website!"
-                    required
-                  />
-                </div>
-                <div>
-                  <button className="submit" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+        <div className="contact__form">
+          <form method="POST" action="https://formspree.io/crbaucom@gmail.com">
+            <div className="field">
+              <label htmlFor="name">Name:</label>
+              <input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Chris Baucom"
+                required
+              />
             </div>
-          </div>
+            <div className="field">
+              <label htmlFor="email">Email: </label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="chris@email.com"
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="phone">Phone: </label>
+              <input
+                id="phone"
+                type="tel"
+                name="phone"
+                placeholder="9035551234"
+                required
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message: </label>
+              <textarea
+                id="message"
+                type="text"
+                name="message"
+                placeholder=""
+                required
+              />
+            </div>
+            <div className="button__wrapper">
+              <button className="submit" type="submit">
+                Send
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     )

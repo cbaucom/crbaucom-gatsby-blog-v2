445a2abd5005777a76e2415b4a7a10e93911bd54
Chris Baucom <crbaucom@gmail.com>
2019-01-30 14:49:04 -0500

    Custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const NotFoundPage = () => (
-  <div className="bieber">
-    <div className="row">
-      <div className="col-md-12 sorry">
-        <h5>Looks like this page is missing.</h5>
-        {/* <Link to={'/'} className="btn btn-primary">
-          Go Home
-        </Link> */}
-      </div>
-    </div>
-  </div>
-)
+import Layout from '../components/Layout'
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <div className="bieber">
+          <div className="row">
+            <div className="col-md-12 sorry">
+              <h5>Looks like this page is missing.</h5>
+              <Link to={'/'} className="btn btn-primary">
+                Go Home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default NotFoundPage

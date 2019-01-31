c2c62a26e48baa776fc74fd1da9024e60e0cdccc
Chris Baucom <crbaucom@gmail.com>
2019-01-30 20:44:23 -0500

    Pass in location

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll'
+import { animateScroll as scroll } from 'react-scroll'
 import Logo from './icons/Logo'
 import Nav from './Nav'
 
@@ -11,11 +11,14 @@ class Header extends Component {
 
   render() {
     return (
-      <nav className="Header navbar navbar-default navbar-fixed-top">
+      <nav
+        className="Header navbar navbar-default navbar-fixed-top"
+        location={this.props.location}
+      >
         {(location.pathname === '/' && (
-          <ScrollLink onClick={this.scrollToTop} className="logo">
+          <div onClick={this.scrollToTop} className="logo">
             <Logo />
-          </ScrollLink>
+          </div>
         )) || (
           <Link to={'/'} className="logo">
             <Logo />

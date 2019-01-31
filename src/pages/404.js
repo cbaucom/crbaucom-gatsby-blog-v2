dfa2b2b23ad0eb78a40f08e141ea1b460cb6cecf
Chris Baucom <crbaucom@gmail.com>
2019-01-30 23:34:27 -0500

    Refactor css to styled components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
+
+const BieberWrapper = styled.div`
+  background-image: url('../img/bieber.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  .sorry {
+    padding: 0.5rem 2rem;
+  }
+`
 
 class NotFoundPage extends React.Component {
   render() {
